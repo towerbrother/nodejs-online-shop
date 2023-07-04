53591606add9f89b5d8fcdd4dd34bf1ed29225d7
towerbrother <giorgio.torre8@gmail.com>
2023-07-04 15:14:08 +0200

    section-5 lecture 65

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product" /><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post('/product', (req, res, next) => {
+  res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Home Page</h1>');
