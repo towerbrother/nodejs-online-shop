db8a4fcd290551304ca906d03dcdad49b6c1bd0b
towerbrother <giorgio.torre8@gmail.com>
2023-07-10 07:35:56 +0200

    served css files statically - end of section 5

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
