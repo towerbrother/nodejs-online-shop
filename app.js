7b00ce6d740c8158bc367ae5c094f6cc82f19925
towerbrother <giorgio.torre8@gmail.com>
2023-07-10 07:15:51 +0200

    served html pages - lecture 71 section 5

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,13 +8,15 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>404 - Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000, () => console.log(`Listening to port 3000`));
+app.listen(port, () => console.log(`Listening to port ${port}`));
