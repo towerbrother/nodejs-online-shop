8a7033cb2046973068e9d08f6e23d8068f82b959
towerbrother <giorgio.torre8@gmail.com>
2023-07-11 07:45:07 +0200

    minor refactoring + sharing data across requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const { routes: adminRoutes } = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./utils/path');
 
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../utils/path');
+const { products } = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js', { products });
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
