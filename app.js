0d66c2e9246398f0814208ac05f31a514b3cac70
Giorgio Torre <giorgio.torre8@gmail.com>
2023-12-03 05:51:11 +0100

    established a connection with mongoDB

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const environment = require('./util/environment');
-const sequelize = require('./util/database');
+// const sequelize = require('./util/database');
+const mongoConnect = require('./util/database');
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -33,55 +34,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      // sequelize object
-      // we call call sequelize methods on the object now stored in the request
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.error(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     // sequelize object
+  //     // we call call sequelize methods on the object now stored in the request
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.error(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// TEMP
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' }); // user creates a product
-User.hasMany(Product);
+// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' }); // user creates a product
+// User.hasMany(Product);
 
-Cart.belongsTo(User);
-User.hasOne(Cart);
+// Cart.belongsTo(User);
+// User.hasOne(Cart);
 
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
 
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
 
-sequelize
-  // .sync({ force: true }) // ONLY DEVELOPMENT
-  .sync()
-  .then(() => {
-    return User.findByPk(1); // dummy user
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: 'Tower', email: 'tower@email.com' });
-    }
+// sequelize
+//   // .sync({ force: true }) // ONLY DEVELOPMENT
+//   .sync()
+//   .then(() => {
+//     return User.findByPk(1); // dummy user
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       return User.create({ name: 'Tower', email: 'tower@email.com' });
+//     }
 
-    return Promise.resolve(user); // not strictly necessary
-  })
-  .then((user) => {
-    console.log(`Created User ${user}`);
-    return user.createCart();
-  })
-  .then((cart) => {
-    console.log(`Created cart`);
-    // we want to run our application only if the connection to the DB is successful
-    // and if a user is there - currently only dummy user with no authentication
-    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-  })
-  .catch((err) => console.error(err));
+//     return Promise.resolve(user); // not strictly necessary
+//   })
+//   .then((user) => {
+//     console.log(`Created User ${user}`);
+//     return user.createCart();
+//   })
+//   .then((cart) => {
+//     console.log(`Created cart`);
+//     // we want to run our application only if the connection to the DB is successful
+//     // and if a user is there - currently only dummy user with no authentication
+//     app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+//   })
+//   .catch((err) => console.error(err));
+
+mongoConnect((client) => {
+  console.log(client);
+  // we want to run our application only if the connection to the DB is successful
+  // and if a user is there - currently only dummy user with no authentication
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+});
--- util/database.js
+++ util/database.js
@@ -1,14 +1,35 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+
+// const environment = require('./environment');
+
+// const {
+//   db: { database, host, user, password },
+// } = environment;
+
+// const sequelize = new Sequelize(database, user, password, {
+//   dialect: 'mysql',
+//   host,
+// });
+
+// module.exports = sequelize;
 
 const environment = require('./environment');
 
 const {
-  db: { database, host, user, password },
+  db: { user, password },
 } = environment;
 
-const sequelize = new Sequelize(database, user, password, {
-  dialect: 'mysql',
-  host,
-});
+const { MongoClient } = require('mongodb');
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://${user}:${password}@cluster0.tueqxki.mongodb.net/?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log('Connected to MongoDB');
+      callback(client);
+    })
+    .catch((err) => console.error(err));
+};
+
+module.exports = mongoConnect;
